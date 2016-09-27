0047a811f92151aaef6b7eacead298039482fb26
Vinod Beloshe <vinodbeloshe12@gmail.com>
2016-09-27 13:31:03 +0530

    changes

--- application/config/database.php
+++ application/config/database.php
@@ -55,10 +55,13 @@ $myserver=$_SERVER['SERVER_NAME'];
 if($myserver!="localhost")
 {
     $db['default']['hostname'] = 'localhost';
-    $db['default']['username'] = 'wohligco_florian';
-    $db['default']['password'] = 'chintan123';
-    $db['default']['database'] = 'wohligco_florian';
-    $db['default']['dbdriver'] = 'mysql';
+    $db['default']['username'] = 'root';
+    $db['default']['password'] = '';
+    $db['default']['database'] = 'florian';
+    // $db['default']['username'] = 'wohligco_florian';
+    // $db['default']['password'] = 'chintan123';
+    // $db['default']['database'] = 'wohligco_florian';
+    $db['default']['dbdriver'] = 'mysqli';
     $db['default']['dbprefix'] = '';
     $db['default']['pconnect'] = TRUE;
     $db['default']['db_debug'] = TRUE;
